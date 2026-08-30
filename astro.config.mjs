cad40b4d72ef24964972b485ea91c024ffc5580b
yigitpekzeren <yigitpekzeren@gmail.com>
2026-08-30 23:59:27 +0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import tailwind from '@astrojs/tailwind';
 import vercel from '@astrojs/vercel/serverless';
 
 export default defineConfig({
-  output: 'server', // Sunucu/API özelliklerini aktif eder (Vercel'de tamamen ücretsizdir)
+  output: 'server',
   adapter: vercel(),
   integrations: [tailwind()]
 });
